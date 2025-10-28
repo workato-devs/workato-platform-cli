9f8be9fb6aac80fcc84748de8094f69520c6b084
Chris Miller <chris.miller@workato.com>
2025-10-28 15:35:24 -0700

    Fix hatch-vcs configuration and use git tags for versioning

--- src/workato_platform_cli/_version.py
+++ src/workato_platform_cli/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.0rc2.dev8'
-__version_tuple__ = version_tuple = (1, 0, 0, 'rc2', 'dev8')
+__version__ = version = '1.0.0rc4.dev0'
+__version_tuple__ = version_tuple = (1, 0, 0, 'rc4', 'dev0')
 
 __commit_id__ = commit_id = None
